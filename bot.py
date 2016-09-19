aab41fc631f51c84bb6af5e9391737abdc4e5edc
midoribaka <midoribaka@users.noreply.github.com>
2016-09-19 23:41:56 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import telebot
+import config
+import random
+
+bot = telebot.TeleBot(config.token)
+
+text = []
+with open('text') as f:
+    text = f.read().splitlines()
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def quote(message):
+    number = random.choice(text)
+    bot.send_message(message.chat.id, number)
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
